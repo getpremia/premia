6eed9e5625d569f141b1004fdba580698fafcec6
Marinus Klasen <marinus@mklasen.nl>
2021-07-21 12:19:58 +0000

    Set to version 0.9.6

--- premia.php
+++ premia.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Premia
  * Plugin URI:        https://mklasen.com
  * Description:       Premia
- * Version:           1.1
+ * Version:           0.9.6
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Marinus Klasen
