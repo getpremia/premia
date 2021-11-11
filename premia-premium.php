0a6d3cf402be19492f286d86687657f7862c8486
Marinus Klasen <marinus@mklasen.nl>
2021-11-11 16:25:44 +0100

    Prepare for free version

---
+++ premia-premium.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Premia
+ *
+ * @package           Premia
+ * @author            Conference7
+ * @copyright         2021 Conference7
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Premia Premium
+ * Plugin URI:        https://mklasen.com
+ * Description:       Premia
+ * Version:           1.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Marinus Klasen
+ * Author URI:        https://getpremia.com
+ * Text Domain:       premia
+ */
+
+namespace Premia;
+
+require plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+/**
+ * Start the plugin
+ *
+ * @since    1.0
+ */
+function run_premia_premium() {
+	new Github();
+	new REST_Endpoints();
+	new Custom_Fields();
+	new Shortcodes();
+	new Debug();
+	new Admin_Options();
+	new Admin_Notices();
+	new Licenses();
+	new Woocommerce_Helper();
+	new Woocommerce_License_Manager_Helper();
+	new Updater( 'premia', __FILE__ );
+}
+
+run_premia_premium();
